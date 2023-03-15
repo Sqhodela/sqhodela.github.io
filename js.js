f3edbe57d6f26f87b626736ccf1790bafeb40dda
Sqhodela <goessler.simon@hak-liezen.at>
2023-03-15 10:36:03 +0100

    yes

--- js.js
+++ js.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-    var a;
          if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
          || navigator.userAgent.match(/iPhone/i)
          || navigator.userAgent.match(/iPad/i)
          || navigator.userAgent.match(/iPod/i)
          || navigator.userAgent.match(/BlackBerry/i)
          || navigator.userAgent.match(/Windows Phone/i)) {
             $(DesktopInUse).hide();
-         } 
-         else if (navigator.userAgent.match(/webOS/i)){
          }
          else {
+            //$("div:not(.yes)").hide();
          }
         });
