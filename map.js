07fd40bad6060796b43b508cafb3753810949a5c
Sqhodela <goessler.simon@hak-liezen.at>
2023-04-17 08:18:27 +0200

    FOR THE CHINESE GOVERMENT

--- map.js
+++ map.js
@@ -1,16 +1,24 @@
-require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate",], function(esriConfig, Map, MapView,Locate) {
 
     esriConfig.apiKey = "AAPKd2b5511cd2544d6894e839880dd45998fa898LLkFa0v45C90oldPCYRRqKVoh7UHEwYXPzInfocrAE7I4PZssJtRFa2lGFN";
 
     const map = new Map({
-      basemap: "arcgis-topographic" // Basemap layer service
+        basemap: "arcgis-navigation"
     });
 
     const view = new MapView({
       map: map,
-      center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
+      center: [14.210, 47.56], // Longitude, latitude
+      zoom: 7, // Zoom level
       container: "viewDiv" // Div element
     });
 
+    const locate = new Locate({
+        view: view,
+        goToOverride: function(view, options) {
+          options.target.scale = 1000;
+          return view.goTo(options.target);
+        }
+      });
+      view.ui.add(locate, "top-left");
   });
